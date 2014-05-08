5cb53bd12b0373c7f0d12ba6e78da6220f1968ca
Simon Shahriveri <si@hi-im-si.com>
2014-05-08 12:40:45 +0100

    initial commit

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,48 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.core.dialogs/www/notification.js",
+        "id": "org.apache.cordova.core.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.AudioHandler/www/MediaError.js",
+        "id": "org.apache.cordova.core.AudioHandler.MediaError",
+        "clobbers": [
+            "window.MediaError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.AudioHandler/www/Media.js",
+        "id": "org.apache.cordova.core.AudioHandler.Media",
+        "clobbers": [
+            "window.Media"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.console/www/console-via-logger.js",
+        "id": "org.apache.cordova.core.console.console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.console/www/logger.js",
+        "id": "org.apache.cordova.core.console.logger",
+        "clobbers": [
+            "cordova.logger"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.phonegap.plugins.PushPlugin": "2.0.1",
+    "org.apache.cordova.core.dialogs": "0.2.0",
+    "org.apache.cordova.core.AudioHandler": "0.2.1",
+    "org.apache.cordova.core.console": "0.2.0"
+}
+// BOTTOM OF METADATA
+});
